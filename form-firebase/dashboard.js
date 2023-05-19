8228e9b9c4a4f43def7e3e729bda7cb349d51ca7
Paul <126140189+RyotaMf@users.noreply.github.com>
2023-05-19 12:04:19 -1100

    Final 50%

---
+++ form-firebase/dashboard.js
@@ -0,0 +1,45 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const cartIcon = document.getElementById('cart-icon');
+  const myCart = document.getElementById('mycart');
+
+  const addToCartButtons = document.querySelectorAll('.add-to-cart');
+
+  addToCartButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      // Get the details of the item
+      const itemContainer = button.parentElement;
+      const image = itemContainer.querySelector('.food-image').src;
+      const title = itemContainer.querySelector('.food-title').textContent;
+      const price = itemContainer.querySelector('.food-price').textContent;
+
+      // Create a new element to display the item in the cart container
+      const newItem = document.createElement('div');
+      newItem.classList.add('cart-item');
+      newItem.innerHTML = `
+        <img src="${image}" alt="${title}" class="cart-image">
+        <span>${title}</span>
+        <br>
+        <span>${price}</span>
+        <br>
+        <label for="${title}-quantity">Quantity:</label>
+        <input type="number" id="${title}-quantity" name="${title}-quantity" min="1" value="1">
+        <button id="check-out">Buy now</button>
+      `;
+     myCart.appendChild(newItem);
+
+    });
+  });
+
+  // Add an event listener to the cart icon to toggle the cart display
+  cartIcon.addEventListener('click', function(event) {
+    event.preventDefault();
+    if (myCart.style.display === 'block') {
+      myCart.style.display = 'none';
+    } else {
+      myCart.style.display = 'block';
+    }
+  });
+});
+
+
+  
